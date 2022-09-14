fc74a35cb64ae38604f748a730f29cf2e4422f32
unknown <leticiadearaujo005@gmail.com>
2022-09-14 10:10:15 -0300

    service

--- backend/farmaciasolidaria/src/main/java/com/generation/farmaciasolidaria/repository/UsuarioRepository.java
+++ backend/farmaciasolidaria/src/main/java/com/generation/farmaciasolidaria/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.generation.farmaciasolidaria.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +14,6 @@ public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
 
 	public List <Usuario> findAllByUsuarioContainingIgnoreCase(@Param("usuario") String usuario);
 
-	
+	public Optional<Usuario> findByUsuario(String usuario);
 	
 }
--- backend/farmaciasolidaria/src/main/java/com/generation/farmaciasolidaria/security/BasicSecurityConfig.java
+++ backend/farmaciasolidaria/src/main/java/com/generation/farmaciasolidaria/security/BasicSecurityConfig.java
@@ -11,9 +11,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvc
 public class BasicSecurityConfig {
 
 	@Bean
